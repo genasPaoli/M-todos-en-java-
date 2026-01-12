5b870a4b23afad50e7fc672128e46151155364ff
Genaro Paoli <mgenasp2403@gmail.com>
2026-01-12 10:54:20 -0600

    FUnciones que no retornan (void)

--- src/metodos.java
+++ src/metodos.java
@@ -7,20 +7,30 @@ public class metodos {
         return num1 + num2;
     }
     public static String info (String nombre, int edad){
-        return "Nombre: " + nombre  + ".Edad: " + edad;
+        return "Nombre: " + nombre  + ". Edad: " + edad;
     }
     public static double sumarDecimales(int num1, int num2) {
         return num1 + num2;
     }
 
-    // Método que no retorna
+    // Método que no retorna}
+
+    public static void asteriscos(){
+        System.out.println("******");
+    }
+    public static void convertir(String mensaje){
+        System.out.println(mensaje.toLowerCase());
+    }
 
     //Método main poara invocar los métodos
     public static void main (String[] args){
         metodos instancia = new metodos();
         System.out.println(instancia.sumar(4,5)); //9
         System.out.println(info("Daniel",  39));
-        System.out.println();
+        System.out.println(sumarDecimales(5,5));
+        asteriscos();
+        convertir("El gEnAS");
+
 
     }
 }
